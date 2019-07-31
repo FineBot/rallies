8f9f429852b71bbfbaca2c6d57a6e5ca5b05a0bc
finebot <kirill.baikin@gmail.com>
2019-07-31 10:01:26 +0300

    Updates

---
+++ precache-manifest.8e8c90afa0cd2ab5a1a6cc8c41cc5ded.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/rallies/static/media/persik.4e1ec840.png"
+  },
+  {
+    "revision": "b59d2e043f5436b77d77",
+    "url": "/rallies/static/js/runtime~main.f328c6d5.js"
+  },
+  {
+    "revision": "2badae560aa44324bc14",
+    "url": "/rallies/static/js/main.5bf40ad0.chunk.js"
+  },
+  {
+    "revision": "4dc371bc0d014b266eb0",
+    "url": "/rallies/static/js/2.f7611a2f.chunk.js"
+  },
+  {
+    "revision": "2badae560aa44324bc14",
+    "url": "/rallies/static/css/main.cbcfab03.chunk.css"
+  },
+  {
+    "revision": "4dc371bc0d014b266eb0",
+    "url": "/rallies/static/css/2.09b63e59.chunk.css"
+  },
+  {
+    "revision": "29535b4604b63f7c084c804403b6438f",
+    "url": "/rallies/index.html"
+  }
+];
