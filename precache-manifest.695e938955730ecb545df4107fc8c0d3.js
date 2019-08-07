5477201a77f461af4d5ab48f522c5b9206f3a4e6
finebot <kirill.baikin@gmail.com>
2019-08-07 10:39:42 +0300

    Updates

--- precache-manifest.695e938955730ecb545df4107fc8c0d3.js
+++ precache-manifest.695e938955730ecb545df4107fc8c0d3.js
@@ -8,15 +8,15 @@ self.__precacheManifest = [
     "url": "/rallies/static/js/runtime~main.f328c6d5.js"
   },
   {
-    "revision": "9a3a4f5063c0f88da3bc",
-    "url": "/rallies/static/js/main.1803a3b7.chunk.js"
+    "revision": "fb2af414de27e6522993",
+    "url": "/rallies/static/js/main.5c3cb379.chunk.js"
   },
   {
     "revision": "6f5532f619b508b2aaa3",
     "url": "/rallies/static/js/2.0361adba.chunk.js"
   },
   {
-    "revision": "9a3a4f5063c0f88da3bc",
+    "revision": "fb2af414de27e6522993",
     "url": "/rallies/static/css/main.cbcfab03.chunk.css"
   },
   {
@@ -24,7 +24,7 @@ self.__precacheManifest = [
     "url": "/rallies/static/css/2.09b63e59.chunk.css"
   },
   {
-    "revision": "005931b0c3a0667978363850681a9579",
+    "revision": "fc26cbac1c037578f80a89f5f000adaf",
     "url": "/rallies/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rallies/precache-manifest.e2dbbfb34da3ede8d7dfcc91df5acf2a.js"
+  "/rallies/precache-manifest.695e938955730ecb545df4107fc8c0d3.js"
 );
 
 workbox.clientsClaim();
