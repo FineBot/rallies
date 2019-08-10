09b7f421bcf57dc4eda7ca037980e7f55ee92d10
finebot <kirill.baikin@gmail.com>
2019-08-10 15:53:59 +0300

    Updates

--- precache-manifest.c948476f7354163a54b783fc91b1751a.js
+++ precache-manifest.c948476f7354163a54b783fc91b1751a.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/rallies/static/js/runtime~main.f328c6d5.js"
   },
   {
-    "revision": "ec5d919abaad61e070dc",
-    "url": "/rallies/static/js/main.b62ec429.chunk.js"
+    "revision": "86c82a918af28459c331",
+    "url": "/rallies/static/js/main.10a09233.chunk.js"
   },
   {
-    "revision": "a74694124623d7f53d31",
-    "url": "/rallies/static/js/2.c67bbfcf.chunk.js"
+    "revision": "99a1940c27409b09e06a",
+    "url": "/rallies/static/js/2.bb1e3a50.chunk.js"
   },
   {
-    "revision": "ec5d919abaad61e070dc",
+    "revision": "86c82a918af28459c331",
     "url": "/rallies/static/css/main.cbcfab03.chunk.css"
   },
   {
-    "revision": "a74694124623d7f53d31",
+    "revision": "99a1940c27409b09e06a",
     "url": "/rallies/static/css/2.09b63e59.chunk.css"
   },
   {
-    "revision": "9e027cb2c96afc6b03e2e60175fb8837",
+    "revision": "da3ccf7d9eb716725bdbe4eb15a8c982",
     "url": "/rallies/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/rallies/precache-manifest.03eef0375c7f545544230d07a2491ceb.js"
+  "/rallies/precache-manifest.c948476f7354163a54b783fc91b1751a.js"
 );
 
 workbox.clientsClaim();
