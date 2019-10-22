24a2de42778fd5089d1c973c5862e47857421600
baikinkirill <kirill.baikin@gmail.com>
2019-10-22 23:15:37 +0300

    Updates

---
+++ precache-manifest.bbdd354a0d2ebea7cb95e95d77a2f8f2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad61868116431a4b1ab8875cb790943b",
+    "url": "/rallies/index.html"
+  },
+  {
+    "revision": "3c1e001c310498181242",
+    "url": "/rallies/static/css/2.7214840c.chunk.css"
+  },
+  {
+    "revision": "84d2e66398c9c9959133",
+    "url": "/rallies/static/css/main.d403c20c.chunk.css"
+  },
+  {
+    "revision": "3c1e001c310498181242",
+    "url": "/rallies/static/js/2.a7fb32dd.chunk.js"
+  },
+  {
+    "revision": "84d2e66398c9c9959133",
+    "url": "/rallies/static/js/main.e5f61184.chunk.js"
+  },
+  {
+    "revision": "b8392109e4dcf92f752e",
+    "url": "/rallies/static/js/runtime-main.6403436d.js"
+  },
+  {
+    "revision": "3dff462ce157c3618aef6ffe2d55620a",
+    "url": "/rallies/static/media/1LZwR4l6AtKw.3dff462c.png"
+  },
+  {
+    "revision": "f8f5fd7cd8c91a58b5e57a4e4dd0a887",
+    "url": "/rallies/static/media/LZwR4l6AtKw.f8f5fd7c.jpg"
+  },
+  {
+    "revision": "bc7158f25acabedd069e80abfb2b4715",
+    "url": "/rallies/static/media/ic_launcher.bc7158f2.png"
+  },
+  {
+    "revision": "7a8ddf8b5089cda2b6acbf70136c03e7",
+    "url": "/rallies/static/media/ic_launcher_black.7a8ddf8b.png"
+  },
+  {
+    "revision": "42446fd58b2b0e69d3946a36d437caea",
+    "url": "/rallies/static/media/ic_launcher_light.42446fd5.png"
+  },
+  {
+    "revision": "75dfa1e108bf5abf13cb0f31c4778c98",
+    "url": "/rallies/static/media/l6AtKw.75dfa1e1.png"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/rallies/static/media/persik.4e1ec840.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rallies/precache-manifest.35c1e18499605a78cf680b0c6c2176da.js"
+  "/rallies/precache-manifest.bbdd354a0d2ebea7cb95e95d77a2f8f2.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/rallies/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rallies/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
